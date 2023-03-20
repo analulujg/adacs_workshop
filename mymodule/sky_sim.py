1a074fec96321ba4c482c0557c2f5145a0965178
Ana Lourdes Juarez <analourdesjg17@gmail.com>
2023-03-21 10:41:20 +1100

    new version

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -8,37 +8,43 @@ from random import uniform
 NSRC = 1_000_000
 
 def get_radec():
-    # from wikipedia
+    """
+    Detarminate Andromeda location
+    """
+
     RA = '00:42:44.3'
     DEC = '41:16:09'
-    
-    # convert to decimal degrees
-    D, M, S = DEC.split(':')
-    DEC = int(D)+int(M)/60+float(S)/3600
-    
-    H, M, S = RA.split(':')
-    RA = 15*(int(H)+int(M)/60+float(S)/3600)
-    RA = RA/cos(DEC*pi/180)
 
-    return (RA, DEC)
+    d, m, s = DEC.split(':')
+    dec = int(d)+int(m)/60+float(s)/3600
 
-def make_stars(RA, DEC, NUM_STARS):
-    RAS = []
-    DECS = []
+    h, m, s = RA.split(':')
+    ra = 15*(int(h)+int(m)/60+float(s)/3600)
+    ra = ra/cos(dec*pi/180)
+
+    return (ra, dec)
+
+def make_stars(ra, dec, num_stars):
+    """ 
+    make 1000 stars within 1 degree of Andromeda
+    """
+    ras = []
+    decs = []
     for i in range(NSRC):
-        RAS.append(RA + uniform(-1, 1))
-        DECS.append(DEC + uniform(-1, 1))
-    return(RAS, DECS)
+        ras.append(ra + uniform(-1, 1))
+        decs.append(dec + uniform(-1, 1))
+    return(ras, decs)
 
 def main():
-    RA, DEC = get_radec()
-    RAS, DECS = make_stars(RA,DEC,NSRC)
-    
+    """ main function"""
+    ra, dec = get_radec()
+    ras, decs = make_stars(ra, dec, NSRC)
+
     # now write these to a csv file for use by my other program
     with open('catalog.csv', 'w', encoding='utf-8') as f:
         print("id,ra,dec", file=f)
         for i in range(NSRC):
-            print(f"{i:07d}, {RAS[i]:12f}, {DECS[i]:12f}", file=f)
+            print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
