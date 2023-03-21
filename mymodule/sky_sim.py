6b1e927cfd0526a340616cef788f8eb9e01f3772
Ana Lourdes Juarez <analourdesjg17@gmail.com>
2023-03-21 11:53:48 +1100

    add and interface

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,9 +1,11 @@
-""""
+"""
 Determine Andromeda location in ra/dec degrees
 """
 
 from math import  cos, pi
 from random import uniform
+from typing import Tuple, List
+import argparse
 
 NSRC = 1_000_000
 
@@ -35,13 +37,41 @@ def make_stars(ra, dec, num_stars):
         decs.append(dec + uniform(-1, 1))
     return(ras, decs)
 
+def skysim_parser():
+    """
+    Configure the argparse for skysim
+
+    Returns
+    -------
+    parser : argparse.ArgumentParser
+        The parser for skysim.
+    """
+    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-')
+    parser.add_argument('--ra', dest = 'ra', type=float, default=None,
+                        help="Central ra (degrees) for the simulation location")
+    parser.add_argument('--dec', dest = 'dec', type=float, default=None,
+                        help="Central dec (degrees) for the simulation location")
+    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
+                        help='destination for the output catalog')
+    return parser
+
+
+
+
 def main():
-    """ main function"""
-    ra, dec = get_radec()
+    parser = skysim_parser()
+    options = parser.parse_args()
+    # if ra/dec are not supplied the use a default value
+    if None in [options.ra, options.dec]:
+        ra, dec = get_radec()
+    else:
+        ra = options.ra
+        dec = options.dec
+
     ras, decs = make_stars(ra, dec, NSRC)
 
     # now write these to a csv file for use by my other program
-    with open('catalog.csv', 'w', encoding='utf-8') as f:
+    with open(options.out,'w',encoding='utf-8') as f:
         print("id,ra,dec", file=f)
         for i in range(NSRC):
             print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
